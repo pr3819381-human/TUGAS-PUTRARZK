0377e4d611dbda696e2b1bfeb0479af1711eab9b
putra rizki <pr3819381@gmail.com>
2026-07-27 13:32:13 +0700

    Menambahkan Tugas Javascript Array Dan Objek

---
+++ TUGAS-EVALUASI/index.js
@@ -0,0 +1,19 @@
+let buah = [
+    "Apel",
+    "Mangga",
+    "Jeruk",
+    "Semangka",
+    "Anggur"
+];
+console.log(buah[0]); // Output: Apel
+
+let mahasiswa = {
+    nama: "Putra Rizki",
+    umur: 20,
+    jurusan: "Teknik Informatika",
+    aktif: true
+};
+console.log(mahasiswa.nama); // Output: Putra Rizki
+console.log(mahasiswa.umur); // Output: 20
+console.log(mahasiswa.jurusan); // Output: Teknik Informatika
+console.log(mahasiswa.aktif); // Output: true
